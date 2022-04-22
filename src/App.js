dbc7cb25f92185ab6fb3efb94295343307b3684a
matheuspython <ruivorobado@gmail.com>
2022-04-22 15:09:15 -0300

    primeiro exemplo de componentDidMount

--- src/App.js
+++ src/App.js
@@ -4,26 +4,31 @@ import { Component } from 'react';
 class App extends Component{
     state = {
       posts: [
-        {
-          id: 1,
-          title: 'O titulo 1',
-          body: 'O corpo 1',
-        },
-        {
-          id: 2,
-          title: 'O titulo 2',
-          body: 'O corpo 2',
-        },{
-          id: 3,
-          title: 'O titulo 3',
-          body: 'O corpo 3',
-        },
-      ],
+        
+      ]
 
     }
 
-    componentDidMount() {//executa algo quando o componente for renderizado
+    componentDidMount() {//executa algo quando o componente for renderizado tipo um useEffect
       console.log('componente montado')
+      this.setState({
+        posts: [
+          {
+            id: 1,
+            title: 'O titulo 1',
+            body: 'O corpo 1',
+          },
+          {
+            id: 2,
+            title: 'O titulo 2',
+            body: 'O corpo 2',
+          },{
+            id: 3,
+            title: 'O titulo 3',
+            body: 'O corpo 3',
+          },
+        ],
+      })
     }
 
   render(){
