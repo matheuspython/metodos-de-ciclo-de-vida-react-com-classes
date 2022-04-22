b3f8bd03499f490be39cc5427a44e78856a2dc5b
matheuspython <ruivorobado@gmail.com>
2022-04-22 15:23:59 -0300

    mudando conteudo de um post

--- src/App.js
+++ src/App.js
@@ -3,42 +3,44 @@ import { Component } from 'react';
 
 class App extends Component{
     state = {
+      counter: 0,
       posts: [
-        
-      ]
+        {
+          id: 1,
+          title: 'O titulo 1',
+          body: 'O corpo 1',
+        },
+        {
+          id: 2,
+          title: 'O titulo 2',
+          body: 'O corpo 2',
+        },{
+          id: 3,
+          title: 'O titulo 3',
+          body: 'O corpo 3',
+        },
+      ],
 
     }
 
     componentDidMount() {//executa algo quando o componente for renderizado tipo um useEffect
-      console.log('componente montado')
-      this.setState({
-        posts: [
-          {
-            id: 1,
-            title: 'O titulo 1',
-            body: 'O corpo 1',
-          },
-          {
-            id: 2,
-            title: 'O titulo 2',
-            body: 'O corpo 2',
-          },{
-            id: 3,
-            title: 'O titulo 3',
-            body: 'O corpo 3',
-          },
-        ],
-      })
+      const { posts, counter } = this.state
+      posts[0].title = 'O titulo mudou'
+      setTimeout(()=>{
+        this.setState({ posts })
+      },5000)
+   
     }
 
   render(){
-    const { posts } = this.state
+    const { posts, counter } = this.state
     return (
       <div className="App">
         {posts.map(post => (
-          <div key={post.id}>
-            <h1 key={post.id}>{post.title}</h1>
-            <p>{post.body}</p>
+          <div key={ post.id }>
+            <h1>{ counter }</h1>
+            <h1 key={ post.id }>{post.title}</h1>
+            <p>{ post.body }</p>
           </div>
         ))}
      </div>
